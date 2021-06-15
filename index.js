010a0156159a8ac889e5444ea711fbc965089cb0
Adrien Carpentier <moi@adriencarpentier.com>
2021-06-16 01:08:10 +0900

    Refactor: score and lives calculated in backend

--- index.js
+++ index.js
@@ -2,66 +2,74 @@ const host = "https://non-intelligence-api.herokuapp.com"
 // const host = "http://127.0.0.1:8000"
 
 var currentQuestionId = null
-var score = 0
-var lives = 3
 var clickable = true
+var sessionUid
 
 window.addEventListener('load', function () {
 
-    document.getElementById("score").innerHTML = score
-    document.getElementById("lives").innerHTML = lives
-    getNewQuestion(aggressive)
-
-    document.querySelectorAll('.button').forEach(button => {
-        button.addEventListener('click', function () {
-            if (clickable) {
-
-                clickable = false
-
-                document.getElementById("question-wrapper").style.display = "none"
-
-                if (button.id == "right") {
-                    var answerId = 1
-                }
-                else if (button.id == "left") {
-                    var answerId = 0
-                }
-
-                sendAnswer(currentQuestionId, answerId)
-
-            }
-        })
-    })
-
     // Listen for next button clicked
-    document.getElementById('next-question-button').addEventListener('click', function (event) {
-        document.getElementById("question-wrapper").style.display = "none"
-        document.getElementById("answer-wrapper").style.display = "none"
-        document.getElementById("score").innerHTML = score
-        document.getElementById("lives").innerHTML = lives
+    document.getElementById('next-question-button').addEventListener('click', function () {
         getNewQuestion(aggressive)
     })
 
     // Listen for reset game button being clicked
-    document.getElementById('reset-button').addEventListener('click', function (event) {
-        score = 0
-        lives = 3
-        document.getElementById("answer-wrapper").style.display = "none"
-        document.getElementById("next-question-button").style.display = ""
-        document.getElementById("game-over-wrapper").style.display = "none"
-        document.getElementById("score").innerHTML = score
-        document.getElementById("lives").innerHTML = lives
-        getNewQuestion(aggressive)
+    document.getElementById('reset-button').addEventListener('click', function () {
+        startNewGame(aggressive)
     })
+
+    // Listen to answers button being clicked
+    document.querySelectorAll('.button').forEach(button => {
+        button.addEventListener('click', function () {
+            if (clickable) {
+                clickable = false
+                if (button.id == "right") {
+                    var answer = 1
+                }
+                else if (button.id == "left") {
+                    var answer = 0
+                }
+                sendAnswer(sessionUid, currentQuestionId, answer)
+            }
+        })
+    })
+
+    startNewGame(aggressive)
 
 })
 
+function startNewGame(aggressive) {
+    document.getElementById("score").innerHTML = 0
+    document.getElementById("lives").innerHTML = 3
+    document.getElementById("game-over-wrapper").style.display = "none"
+    document.getElementById("next-question-button").style.display = ""
+
+    const url = new URL(host + "/sessions")
+    return fetch(url, {
+            method: 'POST',
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("API error")
+            }
+        })
+        .then((responseJson) => {
+            sessionUid = responseJson.sessionUid
+            getNewQuestion(aggressive)
+        })
+        .catch((error) => {
+            throw new Error("API error")
+        })
+}
+
 
 function getNewQuestion(aggressive) {
-
+    document.getElementById("question-wrapper").style.display = "none"
+    document.getElementById("answer-wrapper").style.display = "none"
     document.getElementById("loader-wrapper").style.display = "block"
 
-    const url = new URL(host + "/comments")
+    const url = new URL(host + "/questions")
     if (aggressive != null) {
         let params = {
             aggressive: aggressive,
@@ -78,11 +86,10 @@ function getNewQuestion(aggressive) {
             }
         })
         .then((responseJson) => {
-            // console.log(responseJson)
             currentQuestionId = responseJson.id
             document.getElementById("loader-wrapper").style.display = "none"
             document.getElementById("answer-wrapper").style.display = "none"
-            document.getElementById("question").innerHTML = responseJson.comment
+            document.getElementById("question").innerHTML = responseJson.question
             document.getElementById("question-wrapper").style.display = "block"
             clickable = true
         })
@@ -92,19 +99,20 @@ function getNewQuestion(aggressive) {
 
 }
 
-function sendAnswer(questionId, answerId) {
+function sendAnswer(sessionUid, questionId, answer) {
 
     document.getElementById("question-wrapper").style.display = "none"
     document.getElementById("loader-wrapper").style.display = "block"
-
     const url = new URL(host + "/answers")
-    let params = {
-        questionId: questionId,
-        answerId: answerId
+    const data = {
+        'sessionUid': sessionUid,
+        'questionId': questionId,
+        'answer': answer
     }
-    url.search = new URLSearchParams(params).toString()
-
-    return fetch(url)
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
         .then((response) => {
             if (response.ok) {
                 return response.json()
@@ -113,10 +121,8 @@ function sendAnswer(questionId, answerId) {
             }
         })
         .then((responseJson) => {
-            // console.log(responseJson)
             document.getElementById("loader-wrapper").style.display = "none"
-            score = score + responseJson.correct
-            document.getElementById("score").innerHTML = score
+            document.getElementById("score").innerHTML = responseJson.score
             document.getElementById("answer-wrapper").style.display = "block"
             let answerDiv = document.getElementById("answer")
             if (responseJson.correct == 1) {
@@ -125,12 +131,10 @@ function sendAnswer(questionId, answerId) {
             } else {
                 answerDiv.style.color = "red"
                 answerDiv.innerHTML = "Wrong guess!"
-                lives = lives - 1
-                document.getElementById("lives").innerHTML = lives
+                document.getElementById("lives").innerHTML = responseJson.lives
             }
-            if (lives == 0) {
-                document.getElementById("next-question-button").style.display = "none"
-                endGame()
+            if (responseJson.lives == 0) {
+                endGame(responseJson.score, responseJson.topScore)
             }
         })
         .catch((error) => {
@@ -140,38 +144,11 @@ function sendAnswer(questionId, answerId) {
 }
 
 
-function endGame() {
+function endGame(score, topScore) {
 
-    document.getElementById("loader-wrapper").style.display = "block"
+    document.getElementById("next-question-button").style.display = "none"
     document.getElementById("game-over-wrapper").style.display = "block"
-    let gameoverDiv = document.getElementById("game-over")
-    gameoverDiv.innerHTML = "Game over<br/>Your score: " + score + "<br/>"
-
-    const url = new URL(host + "/scores")
-    let params = {
-        score: score,
-    }
-    url.search = new URLSearchParams(params).toString()
-
-    fetch(url)
-        .then((response) => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                throw new Error("API error")
-            }
-        })
-        .then((responseJson) => {
-            gameoverDiv.appendChild(
-                document.createTextNode(
-                    "Top score: " + responseJson.maxScore
-                )
-            );
-        })
-
-    document.getElementById("loader-wrapper").style.display = "none"
-
-    // throw new Error("Game finished")
+    document.getElementById("game-over").innerHTML = "Game over<br/>Your score: " + score + "<br/>Top score: " + topScore
 
 }
 
