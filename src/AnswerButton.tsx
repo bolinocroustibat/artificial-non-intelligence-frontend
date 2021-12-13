9dbe849abd688854ab0fa222d2d226763a0682f8
Adrien Carpentier <moi@adriencarpentier.com>
2021-12-13 18:50:37 +0900

    Better typing

--- src/AnswerButton.tsx
+++ src/AnswerButton.tsx
@@ -1,7 +1,12 @@
-export function AnswerButton(props) {
-    if (props.type === "ai") {
-        return "<div class='button'>I think it's<span>🤖</span>AI-GENERATED</div>";
-    } else if (props.type === "human") {
-        return "<div class='button'>I think it's<span>👩</span>HUMAN-GENERATED</div>";
+interface FormatAnswerButtonProps {
+    type: string
+}
+
+function AnswerButton({ type }: FormatAnswerButtonProps): JSX.Element | null {
+    if (type === "ai") {
+        return <div class='button'>I think it\'s<span>🤖</span>AI-GENERATED</div>;
+    } else if (type === "human") {
+        return <div class='button'>I think it\'s<span>👩</span>HUMAN-GENERATED</div>;
     }
+    return null;
 }
