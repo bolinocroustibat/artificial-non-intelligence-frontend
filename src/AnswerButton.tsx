4af45452c7edecffa380c170377a5227a2461d0c
Adrien Carpentier <moi@adriencarpentier.com>
2021-12-13 19:02:29 +0900

    Fix answerButton component css

--- src/AnswerButton.tsx
+++ src/AnswerButton.tsx
@@ -4,9 +4,9 @@ interface FormatAnswerButtonProps {
 
 function AnswerButton({ type }: FormatAnswerButtonProps): JSX.Element | null {
     if (type === "ai") {
-        return <div class='button'>I think it\'s<span>🤖</span>AI-GENERATED</div>;
+        return <div className='button'>I think it\'s<span>🤖</span>AI-GENERATED</div>;
     } else if (type === "human") {
-        return <div class='button'>I think it\'s<span>👩</span>HUMAN-GENERATED</div>;
+        return <div className='button'>I think it\'s<span>👩</span>HUMAN-GENERATED</div>;
     }
     return null;
 }
