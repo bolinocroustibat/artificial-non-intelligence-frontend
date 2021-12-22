946b2c30b58572769ba509bb9f6d077805919103
Adrien Carpentier <moi@adriencarpentier.com>
2021-12-22 15:06:11 +0900

    Add component

---
+++ src/components/AnswerButton/AnswerButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './AnswerButton.module.css';
+
+const AnswerButton = () => (
+  <div className={styles.AnswerButton}>
+    AnswerButton Component
+  </div>
+);
+
+AnswerButton.propTypes = {};
+
+AnswerButton.defaultProps = {};
+
+export default AnswerButton;
