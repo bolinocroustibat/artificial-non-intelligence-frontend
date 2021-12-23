8d74f1704af2fd474e5f9ca718d4782632abc9e1
Adrien Carpentier <moi@adriencarpentier.com>
2021-12-23 17:06:42 +0900

    Add buttons components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/AnswerButton/AnswerButton'
+import AnswerButton from './components/AnswerButton/AnswerButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AnswerButton type="ai"></AnswerButton>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <AnswerButton type="human"></AnswerButton>
     </div>
   );
 }
--- src/components/AnswerButton/AnswerButton.js
+++ src/components/AnswerButton/AnswerButton.js
@@ -2,11 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './AnswerButton.module.css';
 
-const AnswerButton = () => (
-  <div className={styles.AnswerButton}>
-    AnswerButton Component
-  </div>
-);
+const AnswerButton = (props) => {
+  console.log(props.type)
+  if (props.type === "ai") {
+    return (
+      <div className={styles.AnswerButton}>
+        I think it's<span>🤖</span>AI-GENERATED
+      </div>
+    )
+  } else if (props.type === "human") {
+    return (
+      <div className={styles.AnswerButton}>
+        think it's<span>👩</span>HUMAN-GENERATED
+      </div>
+    )
+  };
+
+  return null;
+
+
+};
 
 AnswerButton.propTypes = {};
 
